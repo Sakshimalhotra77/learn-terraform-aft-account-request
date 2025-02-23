10cca061e704125ca930219b558efda64d2f3e0c
Sakshi <sakshimalhotra77@gmail.com>
2025-02-23 12:10:34 +0530

    Append new Terraform module for Sakshi Mendiratta - Sandbox

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,3 +50,28 @@ module "Security" {
 
   account_customizations_name = "Security-Security"
 }
+
+
+module "Sakshi Mendiratta - Sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "malsaksh+second@amazon.com"
+    AccountName               = "Sakshi Mendiratta - Sandbox"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "malsaksh+second@amazon.com"
+    SSOUserFirstName          = "Sakshi"
+    SSOUserLastName           = "Mendiratta"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Sakshi Mendiratta - Sandbox"
+    change_reason       = "Creating new Security account"
+  }
+
+  account_customizations_name = "Security-Sakshi Mendiratta - Sandbox"
+}
